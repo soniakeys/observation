397b701643d9058922b4c177d88c76f413a9ba6f
Sonia Keys <skeys.cfa.harvard.edu>
2015-01-15 23:15:57 +0000

    initial commit

---
+++ observation.go
@@ -0,0 +1,98 @@
+// Public domain.
+
+// Package observation represents ground-based and space-based observations
+// of moving objects against the sky.
+package observation
+
+import (
+	"math"
+
+	"github.com/soniakeys/astro"
+	"github.com/soniakeys/coord"
+)
+
+// VMeas represents a "visual" measurment in units convenient for computations.
+type VMeas struct {
+	Mjd        float64 // time of observation
+	coord.Sphr         // components in radians
+	// The apparent magnitude is represented as somehow normalized to "V."
+	// The actual observed magnitude band is not represented here.
+	Vmag float64
+	// Quality identifier.  Typically simply the 3 character MPC obscode,
+	// but can be any string to associate the measurement with a quality
+	// level.  This identifier is associated with the obserr keyword
+	// in the digest2 config file, for example.
+	Qual string
+}
+
+// VObs is a common interface for ground-based and spaced-based observations
+type VObs interface {
+	// underlying measurement--the actual observation
+	Meas() *VMeas
+	// location of observer relative to the center of the earth
+	EarthObserverVect() coord.Cart
+}
+
+// SiteObs represents an observation from a fixed ground-based observatory.
+// It satisfies the VObs interface.
+type SiteObs struct {
+	VMeas // the observation
+	// Parallax constants determine the observer location relative to the
+	// center of the Earth.
+	Par *ParallaxConst
+}
+
+// Meas satisfies a method of the VObs interface.
+func (o *SiteObs) Meas() *VMeas {
+	return &o.VMeas
+}
+
+// EarthObserverVect satisfies a method of the VObs interface.
+func (o *SiteObs) EarthObserverVect() coord.Cart {
+	sth, cth := math.Sincos(astro.Lst(o.Mjd, o.Par.Longitude))
+	return coord.Cart{
+		X: o.Par.RhoCosPhi * cth,
+		Y: o.Par.RhoCosPhi * sth,
+		Z: o.Par.RhoSinPhi,
+	}
+}
+
+// SatObs represents an observation from an observatory in Earth orbit.
+// It satisfies the VObs interface.
+type SatObs struct {
+	// Sat is typically the 3 character MPC obscode, but is not restricted
+	// to these.
+	Sat    string
+	VMeas             // the observation
+	Offset coord.Cart // offset from center of Earth, in AU
+}
+
+// Meas satisfies a method of the VObs interface.
+func (o *SatObs) Meas() *VMeas { // Implement VObs
+	return &o.VMeas
+}
+
+// EarthObserverVect satisfies a method of the VObs interface.
+func (o *SatObs) EarthObserverVect() (c coord.Cart) {
+	// to do:  use Offset.  need to verify orientation and such.
+	// until then, geocentric is safer.
+	return
+}
+
+// ParallaxConst represents a vector from the center of the Earth.
+type ParallaxConst struct {
+	Longitude float64 // unit is circles
+	RhoCosPhi float64 // unit is AU
+	RhoSinPhi float64 // unit is AU
+}
+
+// ParallaxMap is a mapping from strings to parallax constants.
+// The map key is typically the 3 character MPC obscode, but
+// is not restriced to these and can be anything convenient.
+type ParallaxMap map[string]*ParallaxConst
+
+// Tracklet, a sequence of observations of the same object.
+type Tracklet struct {
+	Desig string
+	Obs   []VObs
+}
---
+++ observation_test.go
@@ -0,0 +1,39 @@
+// Public domain.
+
+package observation_test
+
+import (
+	"bytes"
+	"math"
+	"testing"
+
+	"github.com/soniakeys/coord"
+	"github.com/soniakeys/mpcformat"
+)
+
+func TestSiteObs_EarthObserverVect(t *testing.T) {
+	m, err := mpcformat.ReadObscodeDat(bytes.NewBufferString(`F51 203.744090.936242+0.351541Pan-STARRS 1, Haleakala`))
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	_, o, err := mpcformat.ParseObs80("     K14G49E* C2014 04 09.45004 16 29 34.386+18 18 53.97         19.3 iL~133CF51", m)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	// (desig, VMeas are tested in testObs80)
+
+	eoWant := coord.Cart{
+		X: -3.6643985193887653e-05,
+		Y: -1.5829620927425095e-05,
+		Z: 1.4988032341235873e-05,
+	}
+	eoGot := o.EarthObserverVect()
+	if math.Abs(eoGot.X-eoWant.X) > 1e-15 ||
+		math.Abs(eoGot.Y-eoWant.Y) > 1e-15 ||
+		math.Abs(eoGot.Z-eoWant.Z) > 1e-15 {
+		t.Fatal("VObs.EarthObserverVect() = %+v, want %+v", eoGot, eoWant)
+		return
+	}
+}
